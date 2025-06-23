b6f42c20679a2ba72afff91bfaa5995c14ef76b6
Flamebearer Eno <lambchopprime@gmail.com>
2025-06-23 14:59:54 -0700

    Add /privacy route and PrivacyPolicy component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Login from './Login';
-import SyncMemory from './SyncMemory';
-import MainLayout from './MainLayout';
-import SignUp from './SignUp';  // 👈 Import SignUp
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainLayout from "./MainLayout";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import SyncMemory from "./SyncMemory";
+import PrivacyPolicy from "./PrivacyPolicy";
 
 function App() {
   return (
     <Router>
-      <div className="app-container">
-        <nav className="navbar">
-          <Link to="/">Login</Link> | <Link to="/chat">Chat</Link> | <Link to="/sync">Sync Memory</Link> | <Link to="/signup">Sign Up</Link> {/* 👈 Add SignUp link */}
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/chat" element={<MainLayout />} />
-          <Route path="/sync" element={<SyncMemory />} />
-          <Route path="/signup" element={<SignUp />} />  {/* 👈 Add SignUp route */}
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/chat" element={<MainLayout />} />
+        <Route path="/sync" element={<SyncMemory />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+      </Routes>
     </Router>
   );
 }
